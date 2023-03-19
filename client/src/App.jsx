2b93b5e4e6e2232c1c82efcde56107e5f5f6da4b
John4E656F <jm49andres@gmail.com>
2023-03-19 17:59:14 +0100

    built app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './assets/memories.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Memories
+        </Typography>
+        <img src={memories} alt='memories' height='60' width='60' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify-content='space-between' alignItems='stretch' spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 }
 
-export default App
+export default App;
